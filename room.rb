7e284562550f08c742fa76459d7e13e3386d6bcb
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-02-02 14:23:49 +0000

    adds guest check out and song addition and deletion

--- room.rb
+++ room.rb
@@ -1,11 +1,13 @@
 class Room
 
-  attr_reader :name, :genre
+  attr_reader :name, :genre, :capacity
 
-  def initialize(name, genre)
+  def initialize(name, genre, capcity)
     @name = name
     @genre = genre
+    @capcity = capacity
     @guests = []
+    @songs = []
   end
 
   def total_guests_in_room()
@@ -14,6 +16,22 @@ class Room
 
   def check_in_to_room(guest_name)
     @guests << guest_name
+  end
+
+  def check_out_of_room(guest_name)
+    @guests.delete(guest_name) if @guests.include?(guest_name)
+  end
+
+  def total_songs_in_room()
+    return @songs.length()
+  end
+
+  def add_song_to_room(song)
+    @songs << song
+  end
+
+  def remove_song_from_room(song)
+    @songs.delete(song) if @songs.include?(song)
   end
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,6 +3,7 @@ require("minitest/rg")
 
 require_relative("../room.rb")
 require_relative("../guest.rb")
+require_relative("../song.rb")
 
 class RoomTest < MiniTest::Test
 
@@ -13,9 +14,15 @@ class RoomTest < MiniTest::Test
     @guest4 = Guest.new("Vincent", 30)
     @guest5 = Guest.new("Billy", 17)
 
-    @room1 = Room.new("Rock Room", "rock")
-    @room2 = Room.new("Ballad Room", "ballad")
-    @room3 = Room.new("Pop Room", "pop")
+    @room1 = Room.new("Rock Room", "rock", 10)
+    @room2 = Room.new("Ballad Room", "ballad", 5)
+    @room3 = Room.new("Pop Room", "pop", 3)
+
+    @song1 = Song.new("Highway to Hell", "AC/DC", "rock")
+    @song2 = Song.new("Sweet Child of Mine", "Guns n Roses", "rock")
+    @song3 = Song.new("Freebird", "Lynyrd Skynyrd", "rock")
+    @song4 = Song.new("It's all About Love", "Peter and the Testtube Babies", "ballad")
+    @song5 = Song.new("Happy", "Pharrell", "pop")
   end
 
   def test_returns_room_name()
@@ -27,13 +34,45 @@ class RoomTest < MiniTest::Test
   end
 
   def test_check_number_of_guests_in_room()
-    # Room.total_guests_in_room("Rock Room")
     assert_equal(0, @room1.total_guests_in_room())
   end
 
   def test_guest_can_check_in_to_room()
     @room1.check_in_to_room(@guest1)
     assert_equal(1, @room1.total_guests_in_room())
+  end
+
+  def test_guest_can_check_out_of_room__guest_in_room()
+    @room1.check_in_to_room(@guest1)
+    @room1.check_out_of_room(@guest1)
+    assert_equal(0, @room1.total_guests_in_room())
+  end
+
+  def test_guest_can_check_out_of_room__guest_not_in_room()
+    @room1.check_in_to_room(@guest1)
+    @room1.check_out_of_room(@guest2)
+    assert_equal(1, @room1.total_guests_in_room())
+  end
+
+  def test_number_of_songs_in_room_is_returned()
+    assert_equal(0, @room2.total_songs_in_room)
+  end
+
+  def test_add_song_to_room()
+    @room2.add_song_to_room(@song4)
+    assert_equal(1, @room2.total_songs_in_room)
+  end
+
+  def test_remove_song_from_room__song_in_room()
+    @room2.add_song_to_room(@song4)
+    @room2.remove_song_from_room(@song4)
+    assert_equal(0, @room2.total_songs_in_room)
+  end
+
+  def test_remove_song_from_room__song_not_in_room()
+    @room2.add_song_to_room(@song4)
+    @room2.remove_song_from_room(@song3)
+    assert_equal(1, @room2.total_songs_in_room)
   end
 
 end
