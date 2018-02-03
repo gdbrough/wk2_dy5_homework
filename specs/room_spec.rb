34b3460b9273875a409f4d33e409e25913f49463
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-02-03 13:22:13 +0000

    further amendments

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -4,6 +4,7 @@ require("minitest/rg")
 require_relative("../room.rb")
 require_relative("../guest.rb")
 require_relative("../song.rb")
+require_relative("../tab.rb")
 
 class RoomTest < MiniTest::Test
 
